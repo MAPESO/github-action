6b2dbec3de3d85782a303ec18c1b0e84832ba382
markoz <markdrew53@gmail.com>
2019-08-13 17:29:11 -0500

    Se agrego un parrafo

--- api/index.js
+++ api/index.js
@@ -2,5 +2,8 @@ module.exports = (req, res) => {
   res.status(200);
   res.send(`<heade>
      <h1>Test GitHub Actions</h1>
-  </header>`)
-}
+     <p>
+       Este PR deberia hacer deploy con ▲Now
+     </p>
+  </header>`);
+};
